f2e767ac2161b16912d611d9e287bd501d74af8e
Aishwarya Lakshminarayanan <stickyshoes@whybenormal.org>
2015-09-15 20:48:38 +0200

    adding the sample data set for GC content computation

--- python_village/identifying_unknown_DNA_quickly.py
+++ python_village/identifying_unknown_DNA_quickly.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 
-f = [line.strip() for line in open('sample_text.txt' , 'r').readlines()]
+f = [line.strip() for line in open('gc_content_data.txt' , 'r').readlines()]
 
 def find_gc_content(strand):
     return (strand.count('C') + strand.count('G')) * 100.0 / (len(strand) * 1.0)
